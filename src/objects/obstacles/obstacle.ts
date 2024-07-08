bd5e885cd9a967a095fdee82b4a348a5f8d5c745
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2024-07-08 11:58:54 +0700

    Initial commit

---
+++ src/objects/obstacles/obstacle.ts
@@ -0,0 +1,26 @@
+
+
+class Obstacle extends Phaser.GameObjects.Image {
+  body: Phaser.Physics.Arcade.Body;
+
+  constructor(aParams: IImageConstructor) {
+    super(aParams.scene, aParams.x, aParams.y, aParams.texture);
+
+    this.initImage();
+    this.scene.add.existing(this);
+  }
+
+  private initImage(): void {
+    // image
+    this.setOrigin(0, 0);
+
+    // physics
+    this.scene.physics.world.enable(this);
+    this.body.setImmovable(true);
+  }
+
+  update(): void {}
+}
+
+
+export default Obstacle;
---
+++ src/scenes/game-scene.ts
@@ -0,0 +1,182 @@
+import { GameObjects } from "phaser";
+import Bullet from "../objects/bullet";
+import Enemy from "../objects/enemy";
+import Obstacle from "../objects/obstacles/obstacle";
+import Player from "../objects/player";
+
+
+
+class GameScene extends Phaser.Scene {
+    private map: Phaser.Tilemaps.Tilemap;
+    private tileset: Phaser.Tilemaps.Tileset;
+    private layer: Phaser.Tilemaps.TilemapLayer;
+
+    private player: Player;
+    private enemies: Phaser.GameObjects.Group;
+    private obstacles: Phaser.GameObjects.Group;
+
+    private target: Phaser.Math.Vector2;
+
+    constructor() {
+        super({
+        key: 'GameScene'
+        });
+    }
+
+    init(): void {}
+
+    create(): void {
+        // create tilemap from tiled JSON
+        this.map = this.make.tilemap({ key: 'levelMap' });
+
+        this.tileset = this.map.addTilesetImage('tiles')!;
+        this.layer = this.map.createLayer('tileLayer', this.tileset, 0, 0)!;
+        this.layer.setCollisionByProperty({ collide: true });
+
+        this.obstacles = this.add.group({
+        /*classType: Obstacle,*/
+        runChildUpdate: true
+        });
+
+        this.enemies = this.add.group({
+        /*classType: Enemy*/
+        });
+        this.convertObjects();
+
+        // collider layer and obstacles
+        this.physics.add.collider(this.player, this.layer);
+        this.physics.add.collider(this.player, this.obstacles);
+
+        // collider for bullets
+        this.physics.add.collider(
+            this.player.getBullets(),
+            this.layer,
+            this.bulletHitLayer,
+            undefined,
+            this
+        );
+        
+        this.physics.add.collider(
+            this.player.getBullets(),
+            this.obstacles,
+            this.bulletHitObstacles,
+            undefined,
+            this
+        );
+        
+        this.enemies.getChildren().forEach((enemyGameObject: Phaser.GameObjects.GameObject) => {
+            this.physics.add.overlap(
+                this.player.getBullets(),
+                enemyGameObject,
+                this.playerBulletHitEnemy,
+                undefined,
+                this
+            );
+
+            let enemy = enemyGameObject as Enemy;
+            this.physics.add.overlap(
+                enemy.getBullets(),
+                this.player,
+                this.enemyBulletHitPlayer,
+                undefined
+            );
+        
+            this.physics.add.collider(
+                enemy.getBullets(),
+                this.obstacles,
+                this.bulletHitObstacles,
+                undefined
+            );
+            this.physics.add.collider(
+                enemy.getBullets(),
+                this.layer,
+                this.bulletHitLayer,
+                undefined
+            );
+        }, this);
+
+        this.cameras.main.startFollow(this.player);
+    }
+
+    update(): void {
+        this.player.update();
+
+        this.enemies.getChildren().forEach((enemyGameObject: GameObjects.GameObject) => {
+        let enemy = enemyGameObject as Enemy;
+        enemy.update();
+        if (this.player.active && enemy.active) {
+            var angle = Phaser.Math.Angle.Between(
+            enemy.body.x,
+            enemy.body.y,
+            this.player.body.x,
+            this.player.body.y
+            );
+
+            enemy.getBarrel().angle =
+            (angle + Math.PI / 2) * Phaser.Math.RAD_TO_DEG;
+        }
+        }, this);
+    }
+
+    private convertObjects(): void {
+        // find the object layer in the tilemap named 'objects'
+        const objectLayer = this.map.getObjectLayer('objects');
+        const objects = objectLayer ? objectLayer.objects as any[] : [];
+
+        objects.forEach((object) => {
+        if (object.type === 'player') {
+            this.player = new Player({
+            scene: this,
+            x: object.x,
+            y: object.y,
+            texture: 'tankBlue'
+            });
+        } else if (object.type === 'enemy') {
+            let enemy = new Enemy({
+            scene: this,
+            x: object.x,
+            y: object.y,
+            texture: 'tankRed'
+            });
+
+            this.enemies.add(enemy);
+        } else {
+            let obstacle = new Obstacle({
+            scene: this,
+            x: object.x,
+            y: object.y - 40,
+            texture: object.type
+            });
+
+            this.obstacles.add(obstacle);
+        }
+        });
+    }
+
+    private bulletHitLayer(bullet: Bullet | Phaser.Tilemaps.Tile | Phaser.Types.Physics.Arcade.GameObjectWithBody): void {
+        if (bullet instanceof Bullet) {
+            bullet.destroy();
+        }
+    }
+
+    private bulletHitObstacles(bullet: Bullet |  Phaser.Tilemaps.Tile | Phaser.Types.Physics.Arcade.GameObjectWithBody, obstacle: Obstacle |  Phaser.Tilemaps.Tile | Phaser.Types.Physics.Arcade.GameObjectWithBody): void {
+        bullet.destroy();
+    }
+
+    private enemyBulletHitPlayer(bullet: Bullet |  Phaser.Tilemaps.Tile | Phaser.Types.Physics.Arcade.GameObjectWithBody, player: Player |  Phaser.Tilemaps.Tile | Phaser.Types.Physics.Arcade.GameObjectWithBody): void {
+        if (player instanceof Player) {
+            bullet.destroy();
+            player.updateHealth();
+        }
+    }
+
+    private playerBulletHitEnemy(bullet : Bullet |  Phaser.Tilemaps.Tile | Phaser.Types.Physics.Arcade.GameObjectWithBody, enemy: Enemy | Phaser.Tilemaps.Tile | Phaser.Types.Physics.Arcade.GameObjectWithBody): void {
+        if (enemy instanceof Enemy) {
+            bullet.destroy();
+            enemy.updateHealth();
+        }
+    }
+}
+
+
+export default GameScene;
